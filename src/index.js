ccd11dba12dd9ec14f215e68b9e1e408b30e825d
NadavPeled1998 <peled.nadav@gmail.com>
2020-02-19 17:15:08 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -12,34 +12,6 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
-
-/*const increment = () => {
-    return{
-        type: 'INCREMENT'
-    }
-}
-const decrement = () => {
-    return{
-        type: 'DECREMENT'
-    }
-}
-const counter = (state = 0, action) => {
-    switch(action.type){
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-    }
-}
-let store = createStore(counter)
-
-store.subscribe(() => console.log(store.getState()))
-
-store.dispatch(increment())*/
-
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
