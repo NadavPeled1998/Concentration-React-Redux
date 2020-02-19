819b9240c9ef0e60ae17839ff104de93c713e9fa
NadavPeled1998 <peled.nadav@gmail.com>
2020-02-19 17:22:47 +0200

    first commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {addFlippedCard, resetFlippedCards} from '../actions/flippedCards';
 import {countTurns} from '../actions/turns';
